31a9f51fc46fd8153c6d7654dcf3a810ed5300a6
Joe Harrison <whiplashomega@gmail.com>
2019-01-14 16:39:35 +0000

    first production build

---
+++ voicestudioapp/dist/precache-manifest.b59982ebc3a32199da9835d40f4a7374.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "31a1ae5851387f74cc9b",
+    "url": "/js/chunk-vendors.b7efa852.js"
+  },
+  {
+    "revision": "ece734aa37dc2a8d2102",
+    "url": "/js/app.e8e7190b.js"
+  },
+  {
+    "revision": "7ec7502e494bf496574b",
+    "url": "/js/about.5273d30c.js"
+  },
+  {
+    "revision": "1d59f7ac6971efdee68c60afd8465035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c45a1a77eaa7536ada7594a4ef742bd9",
+    "url": "/img/logo.c45a1a77.png"
+  },
+  {
+    "revision": "ece734aa37dc2a8d2102",
+    "url": "/css/app.d8d645d5.css"
+  }
+];
